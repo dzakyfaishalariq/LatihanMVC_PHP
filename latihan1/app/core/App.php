895626ec60e261b08ec06fefdb162cd1bfeba919
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-03-03 14:21:58 +0700

    mvc version 1.3"

--- latihan1/app/core/App.php
+++ latihan1/app/core/App.php
@@ -4,6 +4,22 @@ class App
 {
     public function __construct()
     {
-        echo 'oke siappppp';
+        var_dump($this->parseURL());
+    }
+    public function parseURL()
+    {
+        // code...
+        // mengambil url
+        if (isset($_GET['url'])) {
+            // menghilangkan tanda slash di akhir url
+            $url = rtrim($_GET['url']);
+            // menghilangkan tanda slash di awal url
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // menghilangkan tanda slash di awal dan akhir url
+            $url = explode('/', $url);
+            return $url;
+        } else {
+            $url = '';
+        }
     }
 }
